624206ccda46588293bbf03c53dee219742884d5
jackywooks <146035892+jackywooks@users.noreply.github.com>
2024-04-13 22:43:22 -0400

    Initial Build

---
+++ templates/footer.php
@@ -0,0 +1,5 @@
+</div> <!-- End div.main-contnet --> </div> <!-- End div#container -->
+<footer>Developed By: Group 10: Carlos Aragundi Rivas, Erick Ovando Perez, and Jacky Woo</footer>
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE HTML> <!-- Using full HTML 5 -->
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>PHP Final Assignment</title>
+    <meta name="author" content="Carlos Aragundi Rivas, Erick Ovando Perez, Jacky Woo">
+    <meta name="description" content="Final PHP Assignment">
+    <meta name="keywords" content="PHP">
+    <link rel="icon" type="image/x-icon" href="/images/icon.png">
+    <!-- Roboto Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <!-- Github Icon -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <!-- Styles -->
+    <link rel="stylesheet" href="./styles/styles.css">
+</head>
+
+<body>
+    <div id="container">
+        <header id="banner">
+            <h1>Final Assignment</h1>
+            <h3>Users' Info Using PHP with MySQL</h3>
+        </header>
+        <div id="nav">
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="member.php">Member</a></li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="logout.php">Logout</a></li>
+                <li><a href="contact.php">Contact</a></li>
+            </ul>
+        </div>
+        <div class="main-content">
